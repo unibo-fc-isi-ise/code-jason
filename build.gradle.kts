3aab211b7a12aba9012c048120cb0a24d61cc4a1
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2024-03-25 18:10:31 +0100

    fix: dependencies from mcr

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,26 @@
+import org.gradle.configurationcache.extensions.capitalized
+
 plugins {
     java
 }
 
 allprojects {
+    apply<JavaPlugin>()
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(17)
+        }
+    }
+
     repositories {
         mavenCentral()
-        maven("http://jacamo.sourceforge.net/maven2") {
-            isAllowInsecureProtocol = true
-        }
-        maven("https://jade.tilab.com/maven")
     }
 
     group = "it.unibo.ise"
 }
 
 subprojects {
-
-    apply<JavaPlugin>()
-
     sourceSets {
         main {
             resources {
@@ -27,23 +30,18 @@ subprojects {
     }
 
     dependencies {
-        implementation("org.jason-lang", "jason", "2.5.2")
+        implementation("io.github.jason-lang:interpreter:3.2.0")
         testImplementation("junit", "junit", "4.13.2")
     }
 
-    configure<JavaPluginConvention> {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-    }
-
-
     file(projectDir).listFiles().filter { it.extension == "mas2j" }.forEach { mas2jFile ->
-        task<JavaExec>("run${mas2jFile.nameWithoutExtension.capitalize()}Mas") {
+        task<JavaExec>("run${mas2jFile.nameWithoutExtension.capitalized()}Mas") {
             group = "run"
             classpath = sourceSets.getByName("main").runtimeClasspath
             mainClass.set("jason.infra.centralised.RunCentralisedMAS")
             args(mas2jFile.path)
             standardInput = System.`in`
+            javaLauncher.set(javaToolchains.launcherFor(java.toolchain))
         }
     }
 }
-
